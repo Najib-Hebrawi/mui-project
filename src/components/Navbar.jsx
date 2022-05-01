7bfa4b128fb7ceb8a8251cff795ca038d3e4a86f
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-05-01 19:24:10 +0200

    add searchbar and iconbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { AppBar, styled, Toolbar, Typography } from '@mui/material'
+import { Pets } from '@mui/icons-material';
+import { AppBar, Box, styled, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 
 
@@ -8,18 +9,27 @@ import React from 'react'
 const StyledToolbar = styled(Toolbar)({
   display: 'flex',
   justifyContent: 'space-between'
-})
+});
 
+const Search = styled("div")(({theme}) => ({
+  backgroundColor: "white",
+}));
+const Icons = styled(Box)(({theme}) => ({
+  backgroundColor: "white",
+}));
 
 
 
 
 const Navbar = () => {
   return (
-    <AppBar position="stick">
+    <AppBar position="sticky">
       <StyledToolbar>
 
-      <Typography variant="h6">Najib</Typography>
+      <Typography variant="h6" sx={{display:{xs:"none", sm:"block"}}}>Najib</Typography>
+      <Pets sx={{display:{xs:"block", sm:"none"}}}/>
+      <Search>search</Search>
+      <Icons>icons</Icons>
 
       </StyledToolbar>
     </AppBar>
