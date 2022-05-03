3ddf0530c467ca1cb174d4cd9eaf65ee2f947ab7
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-05-03 15:51:14 +0200

    NavBar is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,108 @@
-import { Pets } from '@mui/icons-material';
-import { AppBar, Box, styled, Toolbar, Typography } from '@mui/material'
-import React from 'react'
-
-
-
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  styled,
+  Toolbar,
+  Typography,
+  InputBase,
+  Badge,
+  Avatar,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import React from "react";
 
 // theme is from mui
 const StyledToolbar = styled(Toolbar)({
-  display: 'flex',
-  justifyContent: 'space-between'
+  display: "flex",
+  justifyContent: "space-between",
 });
 
-const Search = styled("div")(({theme}) => ({
+const Search = styled("div")(({ theme }) => ({
   backgroundColor: "white",
-  padding:"0 10px",
+  padding: "0 10px",
   borderRadius: theme.shape.borderRadius,
-  width: "40%"
-}));
-const Icons = styled(Box)(({theme}) => ({
-  backgroundColor: "white",
+  width: "40%",
 }));
 
+const IconsBox = styled(Box)(({ theme }) => ({
+  backgroundColor: "red",
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  // den her code for at vise Icns kun hvis skærmen er sm og up.
+  [theme.breakpoints.up("sm")]: { display: "flex" },
+}));
 
-
+// to show just a image for beskeder nor vi åbner appen med mobilen.
+const UserIconsBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: { display: "none" },
+}));
 
 const Navbar = () => {
+  // her en useState for at lukke og åbner menu.
+  const [open, setOpen] = useState(false);
+
   return (
     <AppBar position="sticky">
       <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Najib
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />{" "}
+        </Search>
 
-      <Typography variant="h6" sx={{display:{xs:"none", sm:"block"}}}>Najib</Typography>
-      <Pets sx={{display:{xs:"block", sm:"none"}}}/>
-      <Search>search</Search>
-      <Icons>icons</Icons>
+        <IconsBox>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
 
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            onClick={(event) => setOpen(true)}
+          />
+        </IconsBox>
+
+        <UserIconsBox onClick={(event) => setOpen(true)}>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+          <Typography variant="span">Najib </Typography>
+        </UserIconsBox>
       </StyledToolbar>
+
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open} // open kom fra vores state
+        onClose={(event) => setOpen(false)} // den her når vi klicker ik på menu så menuen lukker
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
-  )
+  );
 };
 
-export default Navbar
+export default Navbar;
