6aecf241b67da0520f8cb2c9a5b7f8741c652e2b
Najib-Hebrawi <najiphebrawi@hotmail.com>
2022-05-11 14:53:41 +0200

    Add.jsx is done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import Sidebar from './components/Sidebar';
-import Feed from './components/Feed';
-import Rightbar from './components/Rightbar';
-import Navbar from './components/Navbar';
-
-import { Box,  Stack } from '@mui/material';
-
-
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Navbar from "./components/Navbar";
+import Add from "./components/Add";
+import { Box, Stack } from "@mui/material";
 
 function App() {
-
-  
   return (
-
     <Box>
-
-      <Navbar/>
+      <Navbar />
 
       <Stack direction="row" spacing={2} justifyContent="space-between">
-
-      
-      <Sidebar/>
-      <Feed/>
-      <Rightbar/>
-
+        <Sidebar />
+        <Feed />
+        <Rightbar />
       </Stack>
-    
+      <Add/>
     </Box>
   );
 }
-
 
 export default App;
